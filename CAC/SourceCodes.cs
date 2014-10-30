46a09f5e8c4932065e578b1925d7db1a1c7f093a
MTichy <mtichy0@gmail.com>
2014-10-30 15:12:44 +0100

    Created SourceCodes class.

---
+++ CAC/SourceCodes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CAC
+{
+    public static class SourceCodes
+    {
+        private static DirectoryInfo SourceDir;
+
+        public static void setpath(string path)
+         {
+             SourceDir = new DirectoryInfo(path);
+         }
+
+        public static string getpath()
+        {
+            return SourceDir.FullName;
+        }
+
+        public static bool isdirectoryset()
+        {
+            if (SourceDir != null)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
