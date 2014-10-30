035329cbc05478028bcfad0fabf19959234ab78b
MTichy <mtichy0@gmail.com>
2014-10-30 15:26:41 +0100

    Edited setpath method inside SourceCodes class.
    Now it shows FolderBrowserDialog and returns true/false based on dialog result.

--- CAC/SourceCodes.cs
+++ CAC/SourceCodes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Windows.Forms;
 
 namespace CAC
 {
@@ -10,9 +11,18 @@ namespace CAC
     {
         private static DirectoryInfo SourceDir;
 
-        public static void setpath(string path)
+        public static bool setpath()
          {
-             SourceDir = new DirectoryInfo(path);
+             FolderBrowserDialog dialog = new FolderBrowserDialog();
+             DialogResult dialogres = dialog.ShowDialog();
+
+            if (dialogres==DialogResult.OK)
+            {
+                SourceDir = new DirectoryInfo(dialog.SelectedPath);
+                return true;
+            }
+
+            return false;
          }
 
         public static string getpath()
