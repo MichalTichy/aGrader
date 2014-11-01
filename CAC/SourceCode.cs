4ba91203fb01a0df01d407115836d140d434b0b5
MTichy <mtichy0@gmail.com>
2014-11-01 13:15:18 +0100

    Added SourceCode class

---
+++ CAC/SourceCode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAC
+{
+    public class SourceCode
+    {
+        public string Path
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
