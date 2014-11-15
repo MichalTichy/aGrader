f465e2b1340be0b65740f507090a63a90b597205
Michal Tichý <mtichy0@gmail.com>
2014-11-15 23:01:18 +0100

    fixed some text

--- CAC/InputFile.Designer.cs
+++ CAC/InputFile.Designer.cs
@@ -43,9 +43,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(15, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.Size = new System.Drawing.Size(84, 13);
             this.label1.TabIndex = 23;
-            this.label1.Text = "Text";
+            this.label1.Text = "Cesta k souboru";
             // 
             // tbPath
             // 
