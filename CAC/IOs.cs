05bfc1173beda48de9facbbdf7882272b81d22dd
Michal Tichý <mtichy0@gmail.com>
2014-11-26 15:54:04 +0100

    Created IOs.cs
    Class contains List of IOForms

---
+++ CAC/IOs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAC
+{
+    public static class IOs
+    {
+        private static List<dynamic> IOForms = new List<dynamic>();
+
+        public static List<dynamic> getList()
+        {
+            return IOForms;
+        }
+
+        public static void Add(dynamic IOForm)
+        {
+            if (SideFormManager.IsValidDataType(IOForm.GetType()))
+                IOForms.Add(IOForm);
+        }
+
+        public static void Import()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static void Export()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
