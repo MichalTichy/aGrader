af2ee44aa8030787c1d6e1a5985ab6686d5f99b2
Michal Tichý <mtichy0@gmail.com>
2014-12-01 14:30:40 +0100

    Added GenerateXMLDocument method to IOs.cs

--- CAC/IOs.cs
+++ CAC/IOs.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace CAC
 {
@@ -26,6 +27,57 @@ namespace CAC
             IOForms[index2] = temp;
         }
 
+        private static XmlDocument GenerateXMLDocument()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration declaration = doc.CreateXmlDeclaration("1.0", "utf-8", null);
+            doc.AppendChild(declaration);
+            XmlElement root = doc.CreateElement("TestProtocol");
+
+            foreach (dynamic IOForm in IOForms)
+            {
+                XmlElement IOMethod = doc.CreateElement(IOForm.Name);
+                switch (IOForm.Name as string)
+                {
+                    case "InputString":
+                        XmlElement stringvalue = doc.CreateElement("string");
+                        stringvalue.InnerText = IOForm.Text;
+
+                        IOMethod.AppendChild(stringvalue);
+                        break;
+                    case "InputNumber":
+                        XmlElement numericvalue = doc.CreateElement("numeric");
+                        numericvalue.InnerText = IOForm.Value;
+
+                        IOMethod.AppendChild(numericvalue);
+                        break;
+                    case "InputRandomNumber":
+                        XmlElement minvalue = doc.CreateElement("minValue");
+                        minvalue.InnerText = IOForm.min;
+                        XmlElement maxvalue = doc.CreateElement("maxValue");
+                        maxvalue.InnerText = IOForm.max;
+                        XmlElement isDecimal = doc.CreateElement("isDecimal");
+                        isDecimal.InnerText = IOForm.Decimal;
+
+                        IOMethod.AppendChild(minvalue);
+                        IOMethod.AppendChild(maxvalue);
+                        IOMethod.AppendChild(isDecimal);
+                        break;
+                    case "InputFile":
+                        XmlElement path = doc.CreateElement("path");
+                        path.InnerText = IOForm.path;
+                        XmlElement lineformat = doc.CreateElement("lineformat");
+                        lineformat.InnerText = IOForm.lineformat;
+
+                        IOMethod.AppendChild(path);
+                        IOMethod.AppendChild(lineformat);
+                        break;
+                }
+                root.AppendChild(IOMethod);
+            }
+            doc.AppendChild(root);
+            return doc;
+        }
         public static void Import()
         {
             throw new System.NotImplementedException();
@@ -33,7 +85,6 @@ namespace CAC
 
         public static void Export()
         {
-            throw new System.NotImplementedException();
         }
 
     }
