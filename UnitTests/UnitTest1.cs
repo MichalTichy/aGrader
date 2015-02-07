90a37fcac0797ab20910d6127347dba87d709d75
Michal Tichý <mtichy0@gmail.com>
2015-02-07 18:38:26 +0100

    unit tests ...

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CAC;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Rovnice
+    {
+        [TestMethod]
+        public void RovniceScitaniOdcitani()
+        {
+            decimal[] test = { 10, 15, 20 };
+            Assert.AreEqual(new Equation("X0+X1-X2", test).Evaluate(), 5);
+        }
+        [TestMethod]
+        public void RovniceNasobeniDeleni()
+        {
+            decimal[] test = { 10, 15, 20 };
+            Assert.AreEqual(new Equation("X0*X1/X2", test).Evaluate(), 7.5);
+        }
+        [TestMethod]
+        public void RovniceKomplexniPriklad()
+        {
+            decimal[] test = { 10, 15, 20,12,3 };
+            Assert.AreEqual(new Equation("(20+X0)/X1+X2*X3-X4", test).Evaluate(), 239);
+        }
+    }
+}
