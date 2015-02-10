69276df790e46a46ce841c5ccc95fcf37066eca7
Michal Tichý <mtichy0@gmail.com>
2015-02-10 22:12:18 +0100

    implemented EquationValidator

---
+++ CAC/EquationValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CAC
+{
+    public static class EquationValidator
+    {
+        public static bool IsValid(string equation)
+        {
+            decimal[] numbersForEquation = GenerateNumbersForEquation(equation);
+            try
+            {
+                Equation eq=new Equation(equation,numbersForEquation);
+                eq.Evaluate();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static decimal[] GenerateNumbersForEquation(string equation)
+        {
+
+            int countOfUnknownNumbers = GetCountOfUnknownsInEquation(equation);
+            decimal[] numbersForEquation=new decimal[countOfUnknownNumbers];
+
+            for (int i = 0; i < countOfUnknownNumbers; i++)
+            {
+                numbersForEquation[i] = i + 1; //+1 kvůli zamezení případného dělení nulou
+            }
+            return numbersForEquation;
+        }
+
+        private static int GetCountOfUnknownsInEquation(string equation)
+        {
+            int i = 0;
+            while (equation.Contains('X' + i.ToString()))
+                i++;
+            return i+1;
+        }
+    }
+}
