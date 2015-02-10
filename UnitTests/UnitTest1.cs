e86a437dd01928e50322065db668af902858592d
Michal Tichý <mtichy0@gmail.com>
2015-02-10 23:03:21 +0100

    added unit test for equationValidator

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -21,8 +21,17 @@ namespace UnitTests
         [TestMethod]
         public void RovniceKomplexniPriklad()
         {
-            decimal[] test = { 10, 15, 20,12,3 };
+            decimal[] test = { 10, 15, 20, 12, 3 };
             Assert.AreEqual(new Equation("(20+X0)/X1+X2*X3-X4", test).Evaluate(), 239);
+        }
+
+        [TestMethod]
+        public void ValidatorRovnice()
+        {
+            Assert.IsTrue(EquationValidator.IsValid("X0+1*X1"));
+            Assert.IsTrue(EquationValidator.IsValid("25+3"));
+            Assert.IsFalse(EquationValidator.IsValid("invalidstring"));
+
         }
     }
 }
