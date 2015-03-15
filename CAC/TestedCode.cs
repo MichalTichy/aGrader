a162fab4c151d77e7329890eebab545af4fbe524
Michal Tichý <mtichy0@gmail.com>
2015-03-15 21:13:00 +0100

    Added TestedCode.cs

---
+++ CAC/TestedCode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAC
+{
+    public class TestedCode
+    {
+        private Process _app;
+        private readonly TestInstructions _instructions;
+
+        public TestedCode(string path,TestInstructions instructions)
+        {
+            string pathToTCC = Directory.GetCurrentDirectory() + @"\tcc\tcc.exe";
+            _instructions = instructions;
+            _app = new Process
+            {
+                StartInfo =
+                {
+                    FileName = pathToTCC,
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    Arguments = "-run " + @"D:\test.c"
+                }
+            };
+        }
+    }
+}
