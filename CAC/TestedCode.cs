561d92c5a40437f384091bc13184e2aa847ce7f8
Michal Tichý <mtichy0@gmail.com>
2015-03-17 14:48:44 +0100

    added GetError method

--- CAC/TestedCode.cs
+++ CAC/TestedCode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -11,12 +12,24 @@ namespace CAC
     public class TestedCode
     {
         private Process _app;
-        private readonly TestInstructions _instructions;
 
-        public TestedCode(string path,TestInstructions instructions)
+        public event ProgressChangedEventHandler ProgressChanged;
+
+        protected virtual void OnProgressChanged(string message)
+        {
+            if (ProgressChanged != null)
+                ProgressChanged(this, new ProgressChangedEventArgs(0, message));
+        }
+
+        private BackgroundWorker bw = new BackgroundWorker()
+        {
+            WorkerSupportsCancellation = true,
+            WorkerReportsProgress = true
+        };
+
+        public TestedCode(string path)
         {
             string pathToTCC = Directory.GetCurrentDirectory() + @"\tcc\tcc.exe";
-            _instructions = instructions;
             _app = new Process
             {
                 StartInfo =
@@ -26,9 +39,25 @@ namespace CAC
                     RedirectStandardInput = true,
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
-                    Arguments = "-run " + @"D:\test.c"
+                    CreateNoWindow = true,
+                    Arguments = "-run " + path
                 }
             };
+            bw.ProgressChanged += bw_ProgressChanged;
+        }
+
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            OnProgressChanged((string) e.UserState);
+        }
+
+        public string GetError()
+        {
+            _app.Start(); //todo narvat do jineho vlakna (background workeru)
+            string output = _app.StandardError.ReadLine();
+            if(!_app.HasExited)
+                _app.Kill();
+            return output;
         }
     }
 }
