6f81a6b7e35892a69659be32728904dde9141d80
Michal Tichý <mtichy0@gmail.com>
2015-04-08 20:31:06 +0200

    added class to store Results of test

---
+++ CAC/TestResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAC
+{
+    public class TestResult
+    {
+        public readonly string FileName;
+        public readonly string Outputs;
+        public readonly int ProcessorTime;
+        public string Errors { get; private set; }
+        public int[] LinesWithBadOutputs;
+
+        public TestResult(string outputs, string errors, int processorTime, string fileName)
+        {
+            Outputs = outputs;
+            Errors = errors;
+            ProcessorTime = processorTime;
+            FileName = fileName;
+        }
+
+        public void AddError(string errorMsg)
+        {
+            if (Errors.Length != 0)
+                Errors += "\n";
+            Errors += errorMsg;
+        }
+    }
+}
