d1c16c3612d2b0c5552e7b10336d626e68f3b5ae
Michal Tichý <mtichy0@gmail.com>
2015-06-27 22:55:07 +0200

    refactor
    moving things around in InputsOutputs.cs

--- CAC/InputsOutputs.cs
+++ CAC/InputsOutputs.cs
@@ -11,7 +11,6 @@ namespace CAC
     public static class InputsOutputs
     {
         private static List<dynamic> _inOutList = new List<dynamic>();
-        //todo refaktorovat
         public static event EventHandler<EventArgs> InOutListChanged;
 
         public static void OnInOutListChanged()
@@ -19,6 +18,37 @@ namespace CAC
             if (InOutListChanged != null)
                 InOutListChanged(typeof (InputsOutputs), EventArgs.Empty);
         }
+
+        public static void Swap(int index1, int index2)
+        {
+            dynamic temp = _inOutList[index1];
+            _inOutList[index1] = _inOutList[index2];
+            _inOutList[index2] = temp;
+        }
+
+        #region default list operations
+
+        public static void Add(dynamic formIO)
+        {
+            _inOutList.Add(formIO);
+            OnInOutListChanged();
+        }
+
+        public static void Remove(dynamic formIO)
+        {
+            _inOutList.Remove(formIO);
+            OnInOutListChanged();
+        }
+
+        public static void Clear()
+        {
+            _inOutList.Clear();
+            OnInOutListChanged();
+        }
+
+        #endregion
+
+        #region getting list or its members
 
         public static IEnumerable<dynamic> GetList()
         {
@@ -40,29 +70,6 @@ namespace CAC
             return _inOutList.IndexOf(form);
         }
 
-        public static void Add(dynamic formIO)
-        {
-            _inOutList.Add(formIO);
-            OnInOutListChanged();
-        }
-
-        public static void Remove(dynamic formIO)
-        {
-            _inOutList.Remove(formIO);
-            OnInOutListChanged();
-        }
-
-        public static void Swap(int index1, int index2)
-        {
-            dynamic temp = _inOutList[index1];
-            _inOutList[index1] = _inOutList[index2];
-            _inOutList[index2] = temp;
-        }
-
-        public static void Clear()
-        {
-            _inOutList.Clear();
-            OnInOutListChanged();
-        }
+        #endregion
     }
 }
