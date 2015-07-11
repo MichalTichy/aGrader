b6addc70558818280dfd785ce08cb067cf87feee
Michal Tichý <mtichy0@gmail.com>
2015-07-11 18:43:34 +0200

    added few methods to get InputsOutputs list

--- CAC/InputsOutputs.cs
+++ CAC/InputsOutputs.cs
@@ -55,9 +55,27 @@ namespace CAC
             return _inOutList.AsReadOnly();
         }
 
-        public static IEnumerable<dynamic> GetList(Type wantedType)
+        public static IEnumerable<dynamic> GetList(Type wantedType, int maxIndex=-1)
         {
-            return _inOutList.Where(item => item.GetType() == wantedType).ToList();
+            var itemWithinRequestedIndexRange= maxIndex == -1 ? _inOutList : _inOutList.Take(maxIndex - 1);
+            return itemWithinRequestedIndexRange.Where(item => item.GetType() == wantedType);
+        }
+
+        public static IEnumerable<dynamic> GetInputsList(formType type, int maxIndex = -1)
+        {
+            var inputsOutputs = maxIndex == -1 ? _inOutList : _inOutList.Take(maxIndex - 1);
+            var inputsOutputsMatchingSearch =
+                inputsOutputs.Where(t => t.GetType().ToString().Contains(type.ToString()))
+                    .Where(t => t.GetType().ToString().StartsWith("CAC.IO_Forms.Input"));
+            return inputsOutputsMatchingSearch;
+        }
+        public static IEnumerable<dynamic> GetOutputsList(formType type, int maxIndex = -1)
+        {
+            var inputsOutputs = maxIndex == -1 ? _inOutList : _inOutList.Take(maxIndex - 1);
+            var inputsOutputsMatchingSearch =
+                inputsOutputs.Where(t => t.GetType().ToString().Contains(type.ToString()))
+                    .Where(t => t.GetType().ToString().StartsWith("CAC.IO_Forms.Output"));
+            return inputsOutputsMatchingSearch;
         }
 
         public static dynamic GetIOForm(int id)
@@ -72,4 +90,10 @@ namespace CAC
 
         #endregion
     }
+}
+
+public enum formType
+{
+    Number,
+    String,
 }
