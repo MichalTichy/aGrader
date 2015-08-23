4685dd170951cda2277becaf73548a6ae513f9fd
Michal Tichý <mtichy0@gmail.com>
2015-08-23 17:34:59 +0200

    ExceptionsLog class

---
+++ CAC/ExceptionsLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAC
+{
+    public static class ExceptionsLog
+    {
+        public static readonly FileInfo LogFile = new FileInfo(Application.StartupPath+@"\log.txt");
+
+        public static void LogException(string exception)
+        {
+            if (!LogFile.Exists)
+            {
+                LogFile.Create();
+            }
+
+            using (StreamWriter sw=LogFile.AppendText())
+            {
+                sw.WriteLine(DateTime.Now.ToShortTimeString() + " | " +exception);
+            }
+        }
+    }
+}
